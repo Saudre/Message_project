b25443b93f2ee5f784277810d9c2cc7a341408ae
Saudre Cosaat <alejandro.ruedas87@gmail.com>
2021-10-09 16:05:31 +1300

    First Part of the program

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 The project's primary goal is to write a program in JavaScript that generates a new message every time you run it locally to practice and apply the knowledge you got in Git, Github, Markdown, JavaScript, and Node.js. from its path in Full-Stack Software Engineer.
 
 ## Technologies 
+
+* JavaScript
+* Node.js
+* Bash
+* Git
+* GiHub
---
+++ message.js
@@ -0,0 +1,38 @@
+const message = {
+    messages: {
+        Science: [{
+            quote: 'Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.',
+            author: 'Albert Einstein'
+        },{
+            quote: "If you can't explain it simply, you don't understand it well enough.",
+            author: 'Albert Einstein'
+        },{
+            quote: "The good thing about science is that it's true whether or not you believe in it.",
+            author: 'Neil deGrasse Tyson'
+        },{
+            quote: "Science is not only a disciple of reason but also one of romance and passion.",
+            author: "Stephen Hawking"
+        },{
+            quote: "If you want to have good ideas, you must have many ideas.",
+            author: "Linus Pauling"
+        }],
+        History: [{
+            quote: "History is a pack of lies about events that never happened told by people who weren't there.",
+            author: "George Santayana"
+        }],
+        Economy: [],
+        Literature: []
+    },
+    areasKnolege: ['Science', 'History', 'Economy', 'Literature'],
+    getMessage () {
+        const option = Math.floor(Math.random()*this.areasKnolege.length);
+        const area = this.areasKnolege[option];
+        const randomNumber = Math.floor(Math.random()*this.messages[area].length);
+        const messageObject = this.messages[area][randomNumber];
+        return `
+        ${area.toUpperCase()} 
+        ${messageObject.author} ones said that '${messageObject.quote}'`
+    } 
+}
+
+console.log(message.getMessage())
