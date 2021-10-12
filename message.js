dbf866a625a12ec52a5c68eedc57950aa89b836b
Saudre Cosaat <alejandro.ruedas87@gmail.com>
2021-10-12 22:43:35 +1300

    new update

--- message.js
+++ message.js
@@ -19,14 +19,41 @@ const message = {
         History: [{
             quote: "History is a pack of lies about events that never happened told by people who weren't there.",
             author: "George Santayana"
+        },{
+            quote: "Our hisory is not our destiny.",
+            author: "Alan Cohen"
+        },{
+            quote: "Anybody can make history. Only a great man can write it.",
+            author: "Oscar Wild"
+        },{
+            quote: "Human history in essence is the history of ideas.",
+            author: "H. G. Wells"
+        },{
+            quote: "History is the present. That's why every generation writes it anew. But what must people think of as history is its end product, myth.",
+            author: "E. L. Doctorow"
         }],
-        Economy: [],
+        Economy: [{
+            quote: "Economy does not consist in saving the coal, but in using the time while it burns.",
+            author: "Ralph Waldo Emerson"
+        },{
+            quote: "It's a recession when your neighbor loses his job; it's a depression when you lose yours.",
+            author: "Harry S. Truman"
+        },{
+            quote: "Marxism is the opium of the Marxists.",
+            author: "Joan Robinson"
+        },{
+            quote: "Policymakers think that if they get the abstractions right, that will drive behavior in the desired direction. But the world happens in real time.",
+            author: "Sendhil Mullainathan"
+        },{
+            quote: "No gain is so certain as that which proceeds for the economical use of what you already have.",
+            author: "Latin Proverb"
+        }],
         Literature: []
     },
     areasKnowlege: ['Science', 'History', 'Economy', 'Literature'],
     getMessage () {
-        const option = Math.floor(Math.random()*this.areasKnolege.length);
-        const area = this.areasKnolege[option];
+        const option = Math.floor(Math.random()*this.areasKnowlege.length);
+        const area = this.areasKnowlege[option];
         const randomNumber = Math.floor(Math.random()*this.messages[area].length);
         const messageObject = this.messages[area][randomNumber];
         return `${area.toUpperCase()}: ${messageObject.author} ones said that '${messageObject.quote}'`
